576bb8710708157f2124b149da181077fa54fa7b
wiechecdawid <wiechec.dawid@gmail.com>
2020-11-12 22:46:41 +0100

    Initial migration commited

--- SaleOrganizer.Persistence/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace SaleOrganizer.Persistence
-{
-    public class Class1
-    {
-    }
-}
---
+++ SaleOrganizer.Persistence/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SaleOrganizer.Domain;
+using System;
+
+namespace SaleOrganizer.Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        DbSet<Cloth> Clothes { get; set; }
+    }
+}
