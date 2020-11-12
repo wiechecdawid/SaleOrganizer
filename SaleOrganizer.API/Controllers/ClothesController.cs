8bbe04abcf3448cc62004143d84956376992f589
wiechecdawid <wiechec.dawid@gmail.com>
2020-11-12 21:23:34 +0100

    API cleanup + cloth model created

---
+++ SaleOrganizer.API/Controllers/ClothesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SaleOrganizer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClothesController : Controller
+    {
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "cloth1", "cloth2" };
+        }
+    }
+}
--- SaleOrganizer.API/Startup.cs
+++ SaleOrganizer.API/Startup.cs
@@ -36,7 +36,7 @@ namespace SaleOrganizer.API
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
