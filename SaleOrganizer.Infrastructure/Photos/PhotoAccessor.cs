b653d0a66724240a1231d0b7c92615b4a3ef5ff5
Dawid Wiechec <wiechec.dawid@gmail.com>
2021-08-28 18:37:23 +0200

    photo accessor implemented

--- SaleOrganizer.Infrastructure/Photos/PhotoAccessor.cs
+++ SaleOrganizer.Infrastructure/Photos/PhotoAccessor.cs
@@ -1,5 +1,9 @@
+using System;
 using System.Threading.Tasks;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
 using SaleOrganizer.Application.Interfaces;
 using SaleOrganizer.Application.Photos;
 
@@ -7,14 +11,51 @@ namespace SaleOrganizer.Infrastructure.Photos
 {
     public class PhotoAccessor : IPhotoAccessor
     {
-        public Task<PhotoUploadResult> AddPhoto(IFormFile file)
+        private readonly Cloudinary _cloudinary;
+        public PhotoAccessor(IOptions<CloudinarySettings> config)
         {
-            throw new System.NotImplementedException();
+            var account = new Account(
+                config.Value.CloudName,
+                config.Value.ApiKey,
+                config.Value.ApiSecret
+            );
+
+            _cloudinary = new Cloudinary(account);
+        }
+        public async Task<PhotoUploadResult> AddPhoto(IFormFile file)
+        {
+            if(file.Length > 0)
+            {
+                using (var stream = file.OpenReadStream())
+                {
+                    var uploadParams = new ImageUploadParams
+                    {
+                        File = new FileDescription(file.FileName, stream),
+                        Transformation = new Transformation().Height(500).Width(500).Crop("fill")
+                    };
+
+                    var uploadResult = await _cloudinary.UploadAsync(uploadParams);
+
+                    if(uploadResult.Error != null)
+                        throw new Exception(uploadResult.Error.Message);
+
+                        return new PhotoUploadResult
+                        {
+                            PublicId = uploadResult.PublicId,
+                            Url = uploadResult.SecureUrl.ToString()
+                        };
+                }
+            }
+
+            return null;
         }
 
-        public Task<string> DeletePhoto(string publicId)
+        public async Task<string> DeletePhoto(string publicId)
         {
-            throw new System.NotImplementedException();
+            var deletionParams = new DeletionParams(publicId);
+            var result = await _cloudinary.DestroyAsync(deletionParams);
+
+            return result.Result == "ok" ? result.Result : null;
         }
     }
 }
