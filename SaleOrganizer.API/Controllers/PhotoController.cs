345165d43088973bbd5631adcdcecd6d984669bb
Dawid Wiechec <wiechec.dawid@gmail.com>
2021-08-29 14:14:40 +0200

    photocontroller created

---
+++ SaleOrganizer.API/Controllers/PhotoController.cs
@@ -0,0 +1,27 @@
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SaleOrganizer.Application.Photos;
+using SaleOrganizer.Domain;
+
+namespace SaleOrganizer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhotoController: ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public PhotoController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("{clothId}")]
+        public async Task<ActionResult<Photo>> Add([FromQuery]int clothId, [FromForm]Add.Command command)
+        {
+            command.ClothId = clothId;
+            return await _mediator.Send(command);
+        }
+    }
+}
