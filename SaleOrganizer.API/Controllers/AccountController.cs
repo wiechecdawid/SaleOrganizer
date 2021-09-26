b8ca3b7e16fe29e2520420df8d3325608db9c53b
Dawid Wiechec <wiechec.dawid@gmail.com>
2021-09-26 21:43:23 +0200

    GetCurrentUser

--- SaleOrganizer.API/Controllers/AccountController.cs
+++ SaleOrganizer.API/Controllers/AccountController.cs
@@ -1,7 +1,9 @@
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using SaleOrganizer.API.DTOs;
 using SaleOrganizer.API.Services;
 using SaleOrganizer.Domain;
@@ -15,12 +17,12 @@ namespace SaleOrganizer.API.Controllers
     {
         private readonly UserManager<AppUser> _userManager;
         private readonly SignInManager<AppUser> _signInManager;
-        private readonly TokenService _tokenSerice;
+        private readonly TokenService _tokenService;
 
         public AccountController(UserManager<AppUser> userManager,
             SignInManager<AppUser> signInManager, TokenService tokenService)
         {
-            (_userManager, _signInManager, _tokenSerice) = (userManager, signInManager, tokenService);
+            (_userManager, _signInManager, _tokenService) = (userManager, signInManager, tokenService);
         }
 
         [HttpPost("login")]
@@ -36,12 +38,56 @@ namespace SaleOrganizer.API.Controllers
                 return Ok(new UserDto
                 {
                     UserName = user.UserName,
-                    Token = _tokenSerice.CreateToken(user),
+                    Token = _tokenService.CreateToken(user),
                     Email = user.Email
                 });
             }
 
             return Unauthorized();
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+        {
+            if(await _userManager.Users.AnyAsync(x => x.UserName == registerDto.UserName ||
+                x.Email == registerDto.Email))
+            {
+                return BadRequest("User name or email taken");
+            }
+
+            var user = new AppUser
+            {
+                UserName = registerDto.UserName,
+                Email = registerDto.Email
+            };
+
+            var result = await _userManager.CreateAsync(user, registerDto.Password);
+
+            if(result.Succeeded)
+            {
+                return CreateUserObject(user);
+            }
+
+            return BadRequest("something went terribly wrong");
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<ActionResult<UserDto>> GetCurrentUser()
+        {
+            var user = await _userManager.FindByEmailAsync(User.FindFirstValue(ClaimTypes.Email));
+
+            return CreateUserObject(user);
+        }
+
+        private UserDto CreateUserObject(AppUser user)
+        {
+            return new UserDto
+                {
+                    UserName = user.UserName,
+                    Token = _tokenService.CreateToken(user),
+                    Email = user.Email
+                };
         }
     }
 }
--- SaleOrganizer.API/DTOs/RegisterDto.cs
+++ SaleOrganizer.API/DTOs/RegisterDto.cs
@@ -1,9 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace SaleOrganizer.API.DTOs
 {
     public class RegisterDto
     {
+        [Required]
         public string UserName { get; set; }
+
+        [Required]
+        //[RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])$", ErrorMessage = "Hasło musi zawierać dużą i małą literę oraz liczbę")]
         public string Password { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
     }
 }
